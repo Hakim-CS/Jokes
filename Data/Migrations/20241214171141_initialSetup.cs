49e61d240f29c21435d4317df14da89661bbfc08
Hakim NZR <anazary21@posta.pau.edu.tr>
2024-12-24 18:08:39 +0300

    Add files via upload

---
+++ Data/Migrations/20241214171141_initialSetup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "joke",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    jokeQuestion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    jokeDescription = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_joke", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "joke");
+        }
+    }
+}
---
+++ Models/joke.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class joke
+    {
+        public joke()
+        {
+                
+        }
+
+        public int id { get; set; }
+
+        public String jokeQuestion { get; set; }
+        public string jokeDescription { get; set; }
+    }
+}
